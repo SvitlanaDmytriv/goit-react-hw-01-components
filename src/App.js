fb8127ef545992c6e76724d75738a4732cb0bad8
SvitlanaDmytriv <svetagawrylyuk26@gmail.com>
2021-09-29 21:33:36 +0300

    add-markup-fourth-task

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+
 import Profile from './components/Profile/Profile';
 import user from './components/Profile/user.json';
 import StatisticsList from './components/Statistics/StatisticsList';
 import statisticalData from './components/Statistics/statisticalData.json';
 import FriendList from './components/FriendList/FriendList';
 import friends from './components/FriendList/friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/transactions.json';
 
 function App() {
   return (
     <div>
+      Task 1
       <Profile
         name={user.name}
         tag={user.tag}
@@ -16,11 +20,13 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-
+      Task 2
       <StatisticsList title="Upload stats" stat={statisticalData} />
       <StatisticsList stat={statisticalData} />
-
+      Task 3
       <FriendList friendsArr={friends} />
+      Task4
+      <TransactionHistory transactions={transactions} />;
     </div>
   );
 }
